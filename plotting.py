8a27d159ea17e504d4ab49e58a32ece38de97526
snigdhabhagat <32285654+snigdhabhagat@users.noreply.github.com>
2021-09-30 00:24:27 +0530

    Add files via upload

---
+++ plotting.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Bounding box plotting and cropping
+import numpy as np
+from shapely.geometry import Polygon
+def weighted_merge(g, p):
+    g[:8] = (g[8] * g[:8] + p[8] * p[:8])/(g[8] + p[8])
+    g[8] = (g[8] + p[8])
+    return g
+if __name__ == '__main__':
+    # 343,350,448,135,474,143,369,359
+    print(Polygon(np.array([[343, 350], [448, 135],
+                            [474, 143], [369, 359]])).area)
+    
+
